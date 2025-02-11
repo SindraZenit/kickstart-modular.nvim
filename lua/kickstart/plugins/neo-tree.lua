ddfd68dbd5e7f3d3b70bd74a16bfbd634a7b35cf
SindraZenith <pk.casale99@gmail.com>
2025-02-11 16:41:21 +0100

    Major changes in the aesthetic of neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,11 +20,41 @@ return {
         hide_dotfiles = false,
         -- hide_gitignored = true,
       },
+      -- Migliora la visualizzazione della struttura directory
+      indent = {
+        indent_size = 2,
+        padding = 1,
+        with_markers = true,
+        indent_marker = '│',
+        last_indent_marker = '└',
+        highlight = 'NeoTreeIndentMarker',
+      },
+      -- Icone più chiare per cartelle e file
+      icon = {
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = '󰜌',
+      },
+      -- Indicatori di stato Git essenziali
+      git_status = {
+        symbols = {
+          modified = '', -- File modificato
+          added = '', -- File aggiunto
+          deleted = '✖', -- File eliminato
+        },
+      },
       window = {
+        position = 'left',
+        width = 30, -- Larghezza ridotta per una vista più compatta
         mappings = {
           ['\\'] = 'close_window',
         },
       },
+    },
+    -- Mostra le modifiche non salvate
+    modified = {
+      symbol = '●',
+      highlight = 'NeoTreeModified',
     },
   },
 }
