5e81ef46154ae0b47a589468e33226f796a9223a
SindraZenit <pk.casale99@gmail.com>
2025-10-24 11:41:36 +0200

    update: new plugin added vimwiki

---
+++ lua/custom/plugins/vimwiki.lua
@@ -0,0 +1,11 @@
+return {
+  'vimwiki/vimwiki',
+  branch = 'dev',
+
+  config = function()
+    vim.cmd [[
+      let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
+      let g:vimwiki_global_ext = 0
+    ]]
+  end,
+}
